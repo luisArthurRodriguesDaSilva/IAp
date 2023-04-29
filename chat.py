334ce6fc91d19d4dae869bdf37fef68504fe8e67
luisArthurRodriguesDaSilva <luisarthurlards03@gmail.com>
2023-04-29 16:29:34 -0300

    chat tests of 29-4-2023

--- chat.py
+++ chat.py
@@ -1,32 +1,42 @@
+import os
 
-import setuptools
+global_path = "/".join(os.getcwd().split("/")[:3])
+token_path = os.path.join(global_path, "iap_token.txt")
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="iap",
-    version="0.1",
-    author="Luis Arthur Rodrigues da Silva",
-    author_email="luisarthurlards03@gmail.com",
-    description="Um pacote que integra o OpenAI GPT ao seu computador",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/seu-usuario/meu-pacote",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.8',
-    install_requires=[
-        "openai",
-    ],
-    entry_points={
-        "console_scripts": [
-            "iap=iap.main:main",
-            "setToken=iap.helpers:setToken"
-        ],
-    },
-)
+def save_on(path, data):
+    with open(path, "w") as arquivo:
+        arquivo.write(data)
+
+
+def get_py_code(text):
+    return text.split("```python")[1].split("```")[0]
+
+
+def get_code(text):
+    start = text.find("```")
+    end = text.find("```", start + 3)
+    code_block = text[start:end]
+    without_firsth_line = "\n".join(code_block.split("\n")[1:])
+    return without_firsth_line
+
+
+def get_text(file):
+    try:
+        with open(file, "r") as arquivo:
+            return arquivo.read()
+    except FileNotFoundError:
+        return ""
+
+
+def get_token():
+    return get_text(token_path)
+
+
+def set_token():
+    token = input("token: ")
+    save_on(token_path, token)
+    return token
+
+
+save_on("chat1.txt", get_code(get_text("chat.txt")))
