998b45fd7793ae096a707c6888312f5576bae78a
luisArthurRodriguesDaSilva <luisarthurlards03@gmail.com>
2023-04-29 17:37:33 -0300

    getArg on scrapeMe

--- iap/scrapeMe.py
+++ iap/scrapeMe.py
@@ -1,22 +1,23 @@
 import os
-import sys
+from .helpers import getArg
+
 curr_path = os.getcwd()
 initial_text = "chat gpt, saiba que:\n"
 
 def scrap_sistem():
-    extension = 'py'
-    if len(sys.argv) > 1:
-        extension = sys.argv[1].split('.')[-1]
+    extension = getArg(1,'a.py').split('.')[-1]
+
     print(extension)
     messages = [{"role": "system", "content": initial_text}]
     files_info_text = ""
     for dirpath, dirnames, filenames in os.walk(curr_path):
         for filename in filenames:
-            if filename.endswith(f".{extension}"):
+            if filename.endswith(f".{extension}") or filename.endswith(getArg(2,'breakit')):
                 file_path = os.path.join(dirpath, filename)
-                with open(file_path, "r") as arquivo:
-                    conteudo = arquivo.read()
-                    curr_text = f"""
+                try:
+                    with open(file_path, "r") as arquivo:
+                        conteudo = arquivo.read()
+                        curr_text = f"""
 ```python
 O arquivo {file_path} contém:
 
@@ -24,10 +25,12 @@ O arquivo {file_path} contém:
 ```
 
 """
-                    messages.append(
-                        {"role": "user", "content": curr_text},
-                    )
-                    files_info_text += curr_text
+                        messages.append(
+                            {"role": "user", "content": curr_text},
+                        )
+                        files_info_text += curr_text
+                except Exception as e:
+                    print(e)
 
     complete_text = initial_text + files_info_text
     with open("test.txt", "w") as arquivo:
