96cecc7a8929d9d876d844e295a5a7f6b98ee1df
luisArthurRodriguesDaSilva <luisarthurlards03@gmail.com>
2023-04-29 16:28:55 -0300

    main can get path before initialization

--- iap/main.py
+++ iap/main.py
@@ -1,16 +1,22 @@
-from .helpers import save_on, getPyCode, getToken, setToken
+import sys
+from .helpers import save_on, getCode, getToken, setToken
 from .iaApi import send_to_chatGPT, final_text
 
 
 def main():
+    final_file = 'chat.py'
+    if len(sys.argv) > 1:
+        final_file = sys.argv[1]
+
     if not getToken():
         setToken()
 
     prompt = input(f"Digite o prompt: \n{final_text}")
+
     try:
         reply = send_to_chatGPT(prompt)
-        save_on("chat.txt", reply)
-        code = getPyCode(reply)
-        save_on("chat.py", code)
+        save_on('chat.txt', reply)
+        code = getCode(reply)
+        save_on(final_file, code)
     except Exception as e:
         print(e)
