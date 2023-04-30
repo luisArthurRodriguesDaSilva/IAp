03d948bc7b66d961c35af0e203d0d0a13dfab229
luisArthurRodriguesDaSilva <luisarthurlards03@gmail.com>
2023-04-30 20:03:54 -0300

    fix: :bug: endswith in anothe endswith is a problem

--- iap/scrapeMe.py
+++ iap/scrapeMe.py
@@ -6,13 +6,11 @@ initial_text = "chat gpt, saiba que:\n"
 
 def scrap_sistem():
     extension = getArg(1,'a.py').split('.')[-1]
-
-    print(extension)
     messages = [{"role": "system", "content": initial_text}]
     files_info_text = ""
     for dirpath, dirnames, filenames in os.walk(curr_path):
         for filename in filenames:
-            if  filename.endswith(getArg(2,filename.endswith(f".{extension}"))):
+            if  filename.endswith(getArg(2,f".{extension}")):
                 file_path = os.path.join(dirpath, filename)
                 try:
                     with open(file_path, "r") as arquivo:
