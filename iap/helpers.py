e80fb2013757b9473543eb8e089aaab4488d0fa5
luisArthurRodriguesDaSilva <luisarthurlards03@gmail.com>
2023-06-09 00:35:06 -0300

    feat: :sparkles: implemented of dir flag

--- iap/helpers.py
+++ iap/helpers.py
@@ -14,12 +14,13 @@ def getFlagsDict():
     flags = getFlags()
     group = dict()
     for i , flag in enumerate(flags):
+        flagName = flag.replace('--','')
         start = args.index(flag)+1
         end = (len(args) + 1) if i == (len(flags)-1) else args.index(flags[i+1])
         collection = args[start:end]
-        print(flag,collection)
-        group[flag] = []
-        group[flag] += collection
+        group[flagName] = []
+        group[flagName] += collection
+    return group
 
 
 def save_on(path, data):
--- iap/scrapeMe.py
+++ iap/scrapeMe.py
@@ -1,23 +1,29 @@
 import os
-from .helpers import getArg
+from .helpers import getArg, getFlagsDict
 
 curr_path = os.getcwd()
 initial_text = "chat gpt, saiba que:\n"
-
+flags = getFlagsDict()
+print('flags',flags)
 def scrap_sistem():
     extension = getArg(1,'a.py').split('.')[-1]
     messages = [{"role": "system", "content": initial_text}]
     files_info_text = ""
-    for dirpath, dirnames, filenames in os.walk(curr_path):
-        for filename in filenames:
-            if  filename.endswith(getArg(2,f".{extension}")):
-                path_name= f"{'/'.join(dirpath.split('/')[-2:])}/{filename}"
-                print(path_name, 'lido')
-                file_path = os.path.join(dirpath, filename)
-                try:
-                    with open(file_path, "r") as arquivo:
-                        conteudo = arquivo.read()
-                        curr_text = f"""
+    for dir in flags['dirs']:
+        print('dir',dir)
+        for dirpath, dirnames, filenames in os.walk(os.path.join(curr_path, dir)):
+            for filename in filenames:
+                if  filename.endswith(getArg(2,f".{extension}")):
+
+                    path_name:str = f"{dirpath}{filename}"
+                    path_name = path_name.split(curr_path)[1]
+                    
+                    print(path_name, 'lido')
+                    file_path = os.path.join(dirpath, filename)
+                    try:
+                        with open(file_path, "r") as arquivo:
+                            conteudo = arquivo.read()
+                            curr_text = f"""
 ```python
 O arquivo {path_name} contém:
 
@@ -25,14 +31,14 @@ O arquivo {path_name} contém:
 ```
 
 """
-                        messages.append(
-                            {"role": "user", "content": curr_text},
-                        )
-                        files_info_text += curr_text
-                except Exception as e:
-                    print(e)
+                            messages.append(
+                                {"role": "user", "content": curr_text},
+                            )
+                            files_info_text += curr_text
+                    except Exception as e:
+                        print(e)
 
-    complete_text = initial_text + files_info_text
+        complete_text = initial_text + files_info_text
     with open("test.txt", "w") as arquivo:
         arquivo.write(complete_text)
     return messages
